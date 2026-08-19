dc66cce96f922c7397669915a2f43ec7f70020ba
Alan Rocha Pereira <alanrochapereira7@gmail.com>
2026-08-18 22:30:33 -0300

    tabela postagens

---
+++ sql/postagens.sql
@@ -0,0 +1,8 @@
+CREATE TABLE postagens (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    usuario_id INT NOT NULL,
+    conteudo TEXT NOT NULL,
+    data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE
+);
